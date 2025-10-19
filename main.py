46fda84d7fa0c4845fd9ecfd85fccd944aa27828
Vladislav <voytkiv.vladislav@lll.kpi.ua>
2025-10-19 16:52:47 +0300

    feat: include routers

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from fastapi import FastAPI
-from routes import healthcheck, user
+from routes import healthcheck, user, category, record
 
 app = FastAPI()
 
 app.include_router(healthcheck.router)
 app.include_router(user.router)
+app.include_router(category.router)
+app.include_router(record.router)
