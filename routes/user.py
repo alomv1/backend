f1098a0cda8e7004b52d4f9c6210f8a131e97412
Vladislav <voytkiv.vladislav@lll.kpi.ua>
2025-10-19 16:23:25 +0300

    minor changes

--- routes/user.py
+++ routes/user.py
@@ -20,9 +20,9 @@ def get_users():
 
 @router.get('/user/{user_id}', response_model=UserOut, status_code=status.HTTP_200_OK)
 def get_user(user_id):
-    return get_user_service(user_id=user_id)
+    return get_user_service(user_id)
 
 
 @router.delete('/user/user_id', status_code=status.HTTP_204_NO_CONTENT)
 def delete_user(user_id):
-    return delete_user_service(user_id=user_id)
+    return delete_user_service(user_id)
