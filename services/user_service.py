4867ab27f88f136d2337427d9adc0c6c39f8b5ef
Vladislav <voytkiv.vladislav@lll.kpi.ua>
2025-10-19 16:09:03 +0300

    feat: implement user service with CRUD operations

---
+++ services/user_service.py
@@ -0,0 +1,37 @@
+import uuid
+
+from fastapi import status, HTTPException
+
+from schemas.user import UserOut, UserCreate
+
+users = {}
+
+
+def create_user_service(user: UserCreate):
+    user_out = UserOut(name=user.name)
+
+    users[user_out.id] = user_out
+
+    return user_out
+
+
+def get_users_service():
+    return list(users.values())
+
+
+def get_user_service(user_id: uuid.UUID):
+    user = users.get(user_id)
+
+    if not user:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail='User was not found')
+
+    return user
+
+
+def delete_user_service(user_id: uuid.UUID):
+    if user_id not in users:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail='User was not found')
+
+    del users[user_id]
+
+    return
