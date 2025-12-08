c547834d7905a2c87679549ba00edb2059ee404f
vlad <voytkiv.vladislav@lll.kpi.ua>
2025-12-09 00:59:22 +0200

    feat: implement sqlalchemy models

--- alembic/env.py
+++ alembic/env.py
@@ -1,3 +1,5 @@
+import os
+import sys
 from logging.config import fileConfig
 
 from sqlalchemy import engine_from_config
@@ -5,26 +7,26 @@ from sqlalchemy import pool
 
 from alembic import context
 
+from dotenv import load_dotenv
+
+
+load_dotenv()
+
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
 
-# Interpret the config file for Python logging.
-# This line sets up loggers basically.
+
+db_url = os.getenv("DATABASE_URL")
+
+config.set_main_option("sqlalchemy.url", db_url)
+
 if config.config_file_name is not None:
     fileConfig(config.config_file_name)
 
-# add your model's MetaData object here
-# for 'autogenerate' support
-# from myapp import mymodel
-# target_metadata = mymodel.Base.metadata
-target_metadata = None
+from models import * # Або перерахуй моделі: User, Record, Category
 
-# other values from the config, defined by the needs of env.py,
-# can be acquired:
-# my_important_option = config.get_main_option("my_important_option")
-# ... etc.
-
+target_metadata = Base.metadata
 
 def run_migrations_offline() -> None:
     """Run migrations in 'offline' mode.
---
+++ models.py
@@ -0,0 +1,35 @@
+import uuid
+from sqlalchemy import Column, String, ForeignKey, Float
+from sqlalchemy.orm import relationship, Mapped, mapped_column
+from sqlalchemy.dialects.postgresql import UUID
+from database import Base
+
+
+class User(Base):
+    __tablename__ = "users"
+
+    id: Mapped[uuid.UUID] = mapped_column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
+    name: Mapped[str] = mapped_column(String, nullable=False)
+    records = relationship("Record", back_populates="user")
+
+
+class Category(Base):
+    __tablename__ = "categories"
+
+    id: Mapped[uuid.UUID] = mapped_column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
+    name: Mapped[str] = mapped_column(String, nullable=False)
+
+    records = relationship("Record", back_populates="category")
+
+
+class Record(Base):
+    __tablename__ = "records"
+
+    id: Mapped[uuid.UUID] = mapped_column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
+    amount: Mapped[float] = mapped_column(Float, nullable=False)
+
+    user_id: Mapped[uuid.UUID] = mapped_column(ForeignKey("users.id"))
+    category_id: Mapped[uuid.UUID] = mapped_column(ForeignKey("categories.id"))
+
+    user = relationship("User", back_populates="records")
+    category = relationship("Category", back_populates="records")
